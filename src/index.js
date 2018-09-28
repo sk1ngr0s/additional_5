02456023893b472f95de317fcb467688ffc6b0b6
Eugene <levchuk.eugene@gmail.com>
2018-09-28 11:57:47 +0300

    half-solution-additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let realStr = "" + bracketsConfig
+  let bracketsArr = realStr.split(",")
+  let bracketsStr = ""
+  for (let i = 0; i < bracketsArr.length; i++) {
+    bracketsStr += "" + bracketsArr[i]
+  }
+  if (str.replace(bracketsStr, "") == "") {
+    str=str.replace(bracketsStr, "")
+    if (str.length%2===0){
+      return true
+    }
+  }
+  
+  else {
+    return false
+  }
 }
